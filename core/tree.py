1b114a7c6d8f385b26ffca421cf686273810bc55
Nikhil Goyal <nikhilgoyal2704@gmail.com>
2025-06-25 21:51:52 +0530

    remove tree base class

--- core/tree.py
+++ core/tree.py
@@ -1,14 +1,14 @@
-import numpy as np
+# import numpy as np
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-        # self.split_feature = None
-        # self.split_value = None
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+#         # self.split_feature = None
+#         # self.split_value = None
 
-    def __sub__(self, other):
-        return np.sum((self.value - other.value)**2) **0.5
+#     def __sub__(self, other):
+#         return np.sum((self.value - other.value)**2) **0.5
     
-class 
+# class 
